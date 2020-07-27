21bbb06a3cb7168372125ce6c9c46d09cc78c697
Filatau Artsiom <filatau@adani.by>
2020-07-27 16:54:44 +0300

    quick search

---
+++ ExtraLessons1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ExtraLessons1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Массивы -  реализовать сортировку Quicksort");
+
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 30);
+            }
+            PrintArray(array);
+            Console.WriteLine("Меняем местами 0й и последний индекс");
+            int tmp = 0;
+            QuikS(array);
+
+            PrintArray(array);
+
+
+
+
+
+        }
+
+        static public void QuikS(int[] array)
+        {
+            int tmp = 0;
+            int average = array.Length / 2;
+            int p = array[average];
+            Console.WriteLine(p);
+            int i = 0;
+            int j = array.Length - 1;
+            while (i < j)
+            {
+                if (array[i] > p)
+                {
+                    tmp = array[average];
+                    array[average] = array[i];
+                    array[i] = tmp;
+                    average = i;
+                }
+                if (array[j] <= p)
+                {
+                    tmp = array[average];
+                    array[average] = array[j];
+                    array[j] = tmp;
+                    average = j;
+                }
+                i++;
+                j--;
+            }
+        }
+
+        static void QuickSorting(int[] array)
+        {
+            List<int> leftArr = new List<int>();
+            List<int> rightArr = new List<int>();
+            PrintArray(array);
+
+            int a = array[array.Length / 2];
+            int indexLeft = 5;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] <= a)
+                {
+                    leftArr.Add(array[i]);
+                }
+                else
+                    rightArr.Add(array[i]);
+            }
+            if (leftArr.Count > 2)
+            {
+                QuickSorting(leftArr.ToArray());
+            }
+            if (rightArr.Count > 2)
+            {
+                QuickSorting(rightArr.ToArray());
+            }
+        }
+
+        static void PrintArray(int[] mass)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write(mass[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintArray(List<int> mass)
+        {
+            for (int i = 0; i < mass.Count; i++)
+            {
+                Console.Write(mass[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lesson6
 {
@@ -10,11 +11,12 @@ namespace Lesson6
             Queue<string> studentsQueue = new Queue<string>();
             Stack<string> studentsStack = new Stack<string>();
             string[] studensArray = new string[] { "Student One", "Student Two", "Student Three", "Student Four"};
-
+            
             foreach (var student in studensArray)
             {
                 studentsQueue.Enqueue(student);
-                studentsStack.Push(student);
+                Console.WriteLine($"Task {student} is received");
+                //studentsStack.Push(student);
             }
 
             Console.WriteLine("Choose type of queue. 1 - Queue, 2 - Stack");
@@ -22,32 +24,68 @@ namespace Lesson6
 
             if (choosed == 1)
             {
-                Console.WriteLine($"Task from {studentsQueue.Peek()} received");
-                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsQueue.Peek()} received");
-                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsQueue.Peek()} received");
-                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsQueue.Peek()} received");
-                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
-            }
-            else if (choosed == 2)
-            {
-                Console.WriteLine($"Task from {studentsStack.Peek()} received");
-                Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsStack.Peek()} received");
-                Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsStack.Peek()} received");
-                Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
-                Console.WriteLine($"Task from {studentsStack.Peek()} received");
-                Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
+                var b = studentsQueue.Contains("Student Two");
+                Console.WriteLine(b);
+                foreach (var item in studentsQueue)
+                {
+                    Console.WriteLine(item);
+                }
 
+                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
+
+                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
+
+                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
+
+                Console.WriteLine($"{studentsQueue.Dequeue()} got a cup of coffee");
             }
+            //else if (choosed == 2)
+            //{
+            //    Console.WriteLine($"Task from {studentsStack.Peek()} received");
+            //    Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
+            //    Console.WriteLine($"Task from {studentsStack.Peek()} received");
+            //    Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
+            //    Console.WriteLine($"Task from {studentsStack.Peek()} received");
+            //    Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
+            //    Console.WriteLine($"Task from {studentsStack.Peek()} received");
+            //    Console.WriteLine($"{studentsStack.Pop()} got a cup of coffee");
+
+            //}
             else
             {
                 Console.WriteLine("Try again");
             }
-           
+
+            // 
+            string str = "Use for loop to access list as shown below:";
+            string[] strSplitted = str.Split(" ");
+            Console.WriteLine(strSplitted.Length);
+
+            Dictionary<int, int> dictString = new Dictionary<int, int>();
+            for (int i = 0; i < strSplitted.Length; i++)
+            {
+                dictString.Add(i, strSplitted[i].Length);
+
+            }
+            foreach (var x in dictString.OrderBy(x => x.Value))
+            {
+                //Console.WriteLine("{0} - {1}", x.Key, x.Value);
+                Console.WriteLine(strSplitted[x.Key]);
+
+            }
+
+            string strPolindrome = "asd dsa";
+            string sss = "";
+            for (int i = strPolindrome.Length-1; i <= 0; i--)
+            {
+                if (strPolindrome[i] == ' ')
+                {
+                    continue;
+                }
+                sss+=strPolindrome[i];
+            }
+            Console.WriteLine(sss);
+
         }
     }
 }
