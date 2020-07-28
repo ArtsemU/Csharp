d7cfedca109a7e53a5520461c08c641ea6a9394d
Filatau Artsiom <filatau@adani.by>
2020-07-28 16:56:21 +0300

    lesson 7 first step

---
+++ Lesson7/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Lesson7
+{
+    class Student
+    {
+        string FirstName;
+        string LastName;
+        Dictionary<string, int> StudentGrade;
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void SetGrade()
+
+        { 
+            
+        }
+
+
+    }
+}
