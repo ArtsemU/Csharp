6018664f6211f796526b6312f7d25f1ccf042e66
Artsem Filatau <filatau@adani.by>
2020-08-02 15:23:10 +0300

    Не полная версия, ошибка с большими массивами

--- ExtraLessons1/Program.cs
+++ ExtraLessons1/Program.cs
@@ -8,12 +8,12 @@ namespace ExtraLessons1
         {
             Random random = new Random();
             // Создаем массив
-            int[] arr = new int[8];
+            int[] arr = new int[150];
             for (int k = 0; k < arr.Length; k++)
             {
                 arr[k] = random.Next(0, 50);
             }
-            // Выводим в консоли созданный ранее массив
+            //// Выводим в консоли созданный ранее массив
             foreach (var item in arr)
             {
                 Console.Write(item + "  ");
@@ -21,7 +21,7 @@ namespace ExtraLessons1
             Console.WriteLine();
 
             Console.ReadLine();
-            // int[] arrTest1 = new int[5] { 10, 11, 21, 10, 6 };
+            int[] arrTest1 = new int[5] { 8, 3, 1, 7, 6 };
             TestSorting(arr, 0, arr.Length - 1);
 
             // Выводим то что получилось
@@ -38,14 +38,49 @@ namespace ExtraLessons1
             // вот тут начнется методо!
             // последний элемент и есть наш опорный
             int pivot = arr[endIndex];
-            int j = startIndex;
             int i = startIndex;
+            int j = startIndex;
+            Console.WriteLine($"Pivot: {pivot}, startIndex: {startIndex}, endIndex: {endIndex}");
+            while (i < endIndex)
+            {                
+                if (arr[i] < pivot)
+                {
+                    // swop
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j++;
+                }               
+                i++;
+            }
+            int temp2 = arr[endIndex];
+            arr[endIndex] = arr[j];
+            arr[j] = temp2;
+            // рекурсия
+            if (j - startIndex >= 2)
+            {
+                TestSorting(arr, startIndex, j - 1);
+            }
+            if (endIndex - j > 1)
+            {
+                TestSorting(arr, j, endIndex);
+            }
+        }
+
+        static void TestSortingOne(int[] arr, int startIndex, int endIndex)
+        {
+            // вот тут начнется методо!
+            // последний элемент и есть наш опорный
+            int pivot = arr[endIndex];
+            int i = startIndex;
+            int j = startIndex;
+
             while (i < endIndex)
             {
                 if (arr[i] >= pivot)
                 {
                     i++;
-                    //continue;
+                    continue;
                 }
                 else if (arr[i] < pivot)
                 {
