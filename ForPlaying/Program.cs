932bec652ee20aceee45ba29fafe338613524ea2
Artsem Filatau <filatau@adani.by>
2020-09-05 19:39:56 +0300

    Last lesson c#

---
+++ ForPlaying/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForPlaying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>();
+
+            //myList.Add("first item");
+            //myList.Add("second item");
+            //myList.Add("3th item");
+            try
+            {
+                var x = myList.First();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            //Console.WriteLine(x);
+        }
+    }
+}
---
+++ Lesson15/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson15
+{
+    public enum Priority
+    {
+        HIGH,
+        NORMAL
+    }
+
+    public class MyQueue<T>
+    {
+        private List<T> list = new List<T>();
+        public void Enqueue(T item)
+        {
+            list.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (typeof(T) == typeof(int))
+            {
+                var itemInt = list.Cast<int>().FirstOrDefault(i => i > 100);
+                var index = list.Cast<int>().ToList().IndexOf(itemInt);
+                var item = list[index];
+                list.RemoveAt(index);
+                return item;
+            }
+            else
+            {
+                var item = list[0];
+                list.Remove(item);
+                return item;
+            }            
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> myQueue = new MyQueue<int>();
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(50);
+            myQueue.Enqueue(100);
+            myQueue.Enqueue(150);
+
+            var x = myQueue.Dequeue();
+            Console.WriteLine(x);
+
+            x = myQueue.Dequeue();
+            Console.WriteLine(x);
+
+            x = myQueue.Dequeue();
+            Console.WriteLine(x);
+        }
+    }
+}
