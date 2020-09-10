fe7b1e62ae2cb63becd822fa3b7d8253327fab1b
Artsem Filatau <filatau@adani.by>
2020-09-10 20:17:40 +0300

    Tasks 2-5

---
+++ Lesson16/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. Преобразовать его в другое число, цифры которого будут следовать в обратном порядке");
+            NumberRevers(112233);
+            Console.WriteLine();
+
+            Console.WriteLine("3. Заменить все положительные элементы массива на противоположные по знаку, а все отрицательные  на 0");
+            int[] arr3 = new[] { -3, 0, -2, 4, -6, 1 };
+            ChangeNumbers(arr3);
+            Console.WriteLine();
+
+            Console.WriteLine("4. Вывести элементы числового массива, которые больше, чем элементы, стоящие перед ними");
+            int[] arr4 = new[] { 3, 9, 8, 4, 5, 1 };
+            PrintNumberMoreThatPrevious(arr4);
+            Console.WriteLine();
+
+            Console.WriteLine("5. Записать в массив арифметическую и геометрическую прогрессию");
+            // a0 + (n - 1)*b
+            ArithmeticProgression(10, 1, 1);
+            // b1*q^n
+            GeometricProgression(10, 1, 2);
+
+            Console.ReadLine();
+        }
+        public static void Geometricprogression(int n)
+        {
+            //progt i = b0 * 
+        }
+
+        public static List<int> ArithmeticProgression(int n, int a0, int b)
+        {
+            List<int> Arith = new List<int>();
+            Console.WriteLine("ArithmeticProgression");
+            Arith.Add(0);
+            for (int i = 1; i <= n; i++)
+            {
+                Arith.Add(a0 + (i - 1) * b);
+                Console.WriteLine(Arith[i]);
+            }
+            return Arith;
+        }
+
+        public static List<double> GeometricProgression(int n, double b1, double q)
+        {
+            List<double> Geom = new List<double>();
+            Console.WriteLine("GeometricProgression");
+            Geom.Add(1);
+            for (int i = 1; i <= n; i++)
+            {
+                Geom.Add(b1 * Math.Pow(q, i - 1));
+                Console.WriteLine(Geom[i]);
+            }
+            return Geom;
+        }
+
+        public static void PrintNumberMoreThatPrevious(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    Console.WriteLine(arr[i + 1]);
+                }
+            }
+        }
+
+        public static void ChangeNumbers(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    arr[i] *= -1;
+                }
+                else
+                    arr[i] = 0;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void NumberRevers(int num)
+        {
+            string str = num.ToString();
+            string res = "";
+
+            for (int i = str.Length-1; i >=0; i--)
+            {
+                res += str[i];
+            }
+
+            int result = Int32.Parse(res);
+            Console.WriteLine(result);
+        }
+    }
+}
